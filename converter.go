afa37d267cd16e55f5a3a9f9f21f60e4638495d1
Vivian Akinyi <akinyivivian0729@gmail.com>
2026-06-30 13:06:10 +0300

    Implement hexadecimal conversion

--- converter.go
+++ converter.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func ProcessHex(text string) string {
+	words := strings.Fields(text)
+
+	for i := 1; i < len(words); i++ {
+		if words[i] == "(hex)" {
+			hexNumber := words[i-1]
+			number, err := strconv.ParseInt(hexNumber, 16, 64)
+			if err == nil {
+				words[i-1] = strconv.FormatInt(number, 10)
+				words = append(words[:i], words[i+1:]...)
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
+}
--- processor.go
+++ processor.go
@@ -1,5 +1,6 @@
 package main
 
 func ProcessText(text string) string {
+	text = ProcessHex(text)
 	return text
 }
